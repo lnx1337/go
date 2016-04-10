6e296c52d793fd88df981e5a7021927d2ff44919
Jair Peña <dev.lnx1337@gmail.com>
2016-04-09 21:35:46 -0500

    CORS

--- api/encode.go
+++ api/encode.go
@@ -7,9 +7,36 @@ import (
 	"net/http"
 )
 
+const (
+	options           string = "OPTIONS"
+	allow_origin      string = "Access-Control-Allow-Origin"
+	allow_methods     string = "Access-Control-Allow-Methods"
+	allow_headers     string = "Access-Control-Allow-Headers"
+	allow_credentials string = "Access-Control-Allow-Credentials"
+	expose_headers    string = "Access-Control-Expose-Headers"
+	credentials       string = "true"
+	origin            string = "*"
+	methods           string = "POST, GET, OPTIONS, PUT, DELETE, HEAD, PATCH"
+
+	// If you want to expose some other headers add it here
+	headers string = "Accept, Accept-Encoding, Authorization, Content-Length, Content-Type, X-CSRF-Token"
+)
+
+
 func EncodeResponse(w http.ResponseWriter, response interface{}) error {
-	w.Header().Set("Access-Control-Allow-Origin", "*")	
+	if o := r.Header.Get(origin); o != "" {
+		w.Header().Set(allow_origin, o)
+	} else {
+		w.Header().Set(allow_origin, "*")
+	}
+
+		// Set other headers
+	w.Header().Set(allow_headers, headers)
+	w.Header().Set(allow_methods, methods)
+	w.Header().Set(allow_credentials, credentials)
+	w.Header().Set(expose_headers, headers)	
 	w.Header().Set("Content-Type", "application/json")
+	
 	return json.NewEncoder(w).Encode(response)
 }
 
