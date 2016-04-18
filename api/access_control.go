b14da29beff225230849d12d7019d0c04cfe146a
Jair Peña <dev.lnx1337@gmail.com>
2016-04-18 16:07:11 -0500

    Add delete, put method

--- api/access_control.go
+++ api/access_control.go
@@ -7,7 +7,7 @@ import (
 func AccessControl(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
 			return
